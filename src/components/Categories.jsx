69b1fde531c6a269624a99d47661b51fb8a2ac65
ar4ibais <ak.hutov@yandex.ru>
2024-01-31 21:33:11 +0300

    added some layout

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+const Categories = () => {
+    return (
+        <div className="categories">
+            <ul>
+                <li className="active">Все</li>
+                <li>Мясные</li>
+                <li>Морские</li>
+                <li>Вегетерианские</li>
+                <li>Острые</li>
+                <li>Домашние</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
