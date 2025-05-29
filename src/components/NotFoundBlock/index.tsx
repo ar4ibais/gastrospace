ce87646534406d06d7b3cbeed6b5a7777cb24430
Arthur <ak.hutov@yandex.ru>
2025-05-29 21:10:50 +0300

    сделал страницу 404

--- src/components/NotFoundBlock/index.tsx
+++ src/components/NotFoundBlock/index.tsx
@@ -1,17 +1,22 @@
-import { Link } from 'react-router-dom';
-import styles from './NotFoundBlock.module.scss'
+import { Link } from "react-router-dom";
+import image404 from "/404.png";
+import styles from "./NotFoundBlock.module.scss";
 
 const NotFoundBlock = () => {
     return (
         <>
             <div className={styles.root}>
-                <h1>Not Found... </h1>
-                <Link to='/'>
-                    <button style={{ fontSize: 22 }}> Вернуться на главную</button>
+                <img
+                    className={styles.image404}
+                    src={image404}
+                    alt="404 image"
+                />
+                <Link to="/">
+                    <button className="button"> Вернуться на главную</button>
                 </Link>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default NotFoundBlock;
